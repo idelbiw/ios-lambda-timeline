74d1c82b2a70c80d5b82111677357987fb12d3b7
Waseem Idelbi <idelbiw@gmail.com>
2020-09-06 02:36:09 -0400

    added logic to update labels, and update and configure sliders as different filters are set

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 import Photos
 
+enum FilterType {
+    case bokeh
+    case hue
+    case vignette
+    case xray
+    case sepia
+}
+
 class ImagePostViewController: ShiftableViewController {
     
-    //MARK: - Properties and IBOutlets -
+    //MARK: - Properties -
+    
+    var postController: PostController!
+    var post: Post?
+    var imageData: Data?
+    var selectedFilter: FilterType = .bokeh
+    
+    //MARK: - IBOutlets -
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
@@ -19,16 +34,152 @@ class ImagePostViewController: ShiftableViewController {
     @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var postButton: UIBarButtonItem!
     
-    var postController: PostController!
-    var post: Post?
-    var imageData: Data?
+    //Labels - These will display the attribute for the filter that the use will be adjusting
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    @IBOutlet weak var labelThree: UILabel!
+    @IBOutlet weak var labelFour: UILabel!
+    @IBOutlet weak var labelFive: UILabel!
     
-    //MARK: - Methods and IBActions -
+    //Sliders - These are what the user will be interacting with to apply filters
+    @IBOutlet weak var sliderOne   :  UISlider!
+    @IBOutlet weak var sliderTwo   :  UISlider!
+    @IBOutlet weak var sliderThree :  UISlider!
+    @IBOutlet weak var sliderFour  :  UISlider!
+    @IBOutlet weak var sliderFive  :  UISlider!
+    
+    //MARK: - Methods -
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateViews()
         setImageViewHeight(with: 1.0)
+    }
+    
+    //This method handles updating all the control views based on the filter selected
+    private func updateViews() {
+        switch selectedFilter {
+            
+        ///Case for the Bokeh blur filter
+        case .bokeh:
+            
+            //setting up labels and sliders based on selected filter
+            labelOne.text = "Radius"
+            labelTwo.text = "Ring Amount"
+            labelThree.text = "Ring Size"
+            labelFour.text = "Softness"
+            
+            sliderOne.minimumValue = 0
+            sliderOne.maximumValue = 500
+            sliderTwo.minimumValue = 0
+            sliderTwo.maximumValue = 1
+            sliderThree.minimumValue = 0
+            sliderThree.maximumValue = 100
+            sliderFour.minimumValue = 0
+            sliderFour.maximumValue = 10
+            
+            //hiding/revealing views based on selected filter
+            labelOne.isHidden    = false
+            labelTwo.isHidden    = false
+            labelThree.isHidden  = false
+            labelFour.isHidden   = false
+            labelFive.isHidden   = true
+            
+            sliderOne.isHidden   = false
+            sliderTwo.isHidden   = false
+            sliderThree.isHidden = false
+            sliderFour.isHidden  = false
+            sliderFive.isHidden  = true
+            
+        ///Case for the Hue filter
+        case .hue:
+            
+            //setting up labels and sliders based on selected filter
+            labelOne.text = "Angle"
+            
+            sliderOne.minimumValue = 0
+            sliderOne.maximumValue = 90
+            
+            //hiding/revealing views based on selected filter
+            labelOne.isHidden = false
+            labelTwo.isHidden = true
+            labelThree.isHidden = true
+            labelFour.isHidden = true
+            labelFive.isHidden = true
+            
+            sliderOne.isHidden  = false
+            sliderTwo.isHidden = true
+            sliderThree.isHidden = true
+            sliderFour.isHidden = true
+            sliderFive.isHidden = true
+            
+        ///Case for the Vignette filter
+        case .vignette:
+            
+            //setting up labels and sliders based on selected filter
+            labelOne.text = "Intensity"
+            labelTwo.text = "Radius"
+            
+            sliderOne.minimumValue = -1
+            sliderOne.maximumValue = 1
+            sliderTwo.minimumValue = 0
+            sliderTwo.maximumValue = 2
+            
+            //hiding/revealing views based on selected filter
+            labelOne.isHidden = false
+            labelTwo.isHidden = false
+            labelThree.isHidden = true
+            labelFour.isHidden = true
+            labelFive.isHidden = true
+            
+            sliderOne.isHidden  = false
+            sliderTwo.isHidden = false
+            sliderThree.isHidden = true
+            sliderFour.isHidden = true
+            sliderFive.isHidden = true
+            
+        ///Case for the X-Ray filter
+        case .xray:
+            
+            //setting up labels and sliders based on selected filter
+            labelOne.text = "No adjustment needed for this one 🙂"
+            
+            //hiding/revealing views based on selected filter
+            labelOne.isHidden = false
+            labelTwo.isHidden = true
+            labelThree.isHidden = true
+            labelFour.isHidden = true
+            labelFive.isHidden = true
+            
+            sliderOne.isHidden  = true
+            sliderTwo.isHidden = true
+            sliderThree.isHidden = true
+            sliderFour.isHidden = true
+            sliderFive.isHidden = true
+            
+        ///Case for the Vortex Distortion filter
+        case .sepia:
+            
+            //setting up labels and sliders based on selected filter
+            labelOne.text = "Intensity"
+            
+            sliderOne.minimumValue = 0
+            sliderOne.maximumValue = 10
+            
+            //hiding/revealing views based on selected filter
+            labelOne.isHidden = false
+            labelTwo.isHidden = true
+            labelThree.isHidden = true
+            labelFour.isHidden = true
+            labelFive.isHidden = true
+            
+            sliderOne.isHidden  = false
+            sliderTwo.isHidden = true
+            sliderThree.isHidden = true
+            sliderFour.isHidden = true
+            sliderFive.isHidden = true
+        }
     }
     
     private func presentImagePickerController() {
@@ -53,6 +204,25 @@ class ImagePostViewController: ShiftableViewController {
         
         view.layoutSubviews()
     }
+    
+    private func setFilter(To filter: FilterType) {
+        
+        switch filter {
+        case .bokeh:
+            selectedFilter = .bokeh
+        case .hue:
+            selectedFilter = .hue
+        case .vignette:
+            selectedFilter = .vignette
+        case .xray:
+            selectedFilter = .xray
+        case .sepia:
+            selectedFilter = .sepia
+        }
+        
+    }
+    
+    //MARK: - IBActions -
     
     @IBAction func createPost(_ sender: Any) {
         
@@ -99,7 +269,47 @@ class ImagePostViewController: ShiftableViewController {
         presentImagePickerController()
     }
     
-    @IBAction func addFilter(_ sender: Any) {
+    @IBAction func filterChangedSegmentedControl(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            selectedFilter = .bokeh
+            updateViews()
+        case 1:
+            selectedFilter = .hue
+            updateViews()
+        case 2:
+            selectedFilter = .vignette
+            updateViews()
+        case 3:
+            selectedFilter = .xray
+            updateViews()
+        case 4:
+            selectedFilter = .sepia
+            updateViews()
+        default:
+            print("Could not detect selected filter segment")
+        }
+        
+    }
+    
+    @IBAction func sliderOneAdjusted(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func sliderTwoAdjusted(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func sliderThreeAdjusted(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func sliderFourAdjusted(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func sliderFiveAdjusted(_ sender: UISlider) {
         
     }
     
